c14d9463b05c7468c042eb5a38c9b980ee353f76
Isaac Finnegan <isaacfinnegan@gmail.com>
2014-04-14 13:03:58 -0700

    make jira oauth init only happen during onReady

--- integrationlibs/jiraRESTbridge.js
+++ integrationlibs/jiraRESTbridge.js
@@ -1,46 +1,67 @@
-if (NagUI.config.jira.enable) {
+if (NagUI.config.jira.enable)
+{
 
-    Ext.Ajax.request({
-        url: NagUI.config.jira.jiraRESTbridge_path + 'rest/api/latest/project/' + NagUI.config.jira.project_id,
-        method: 'GET',
-        failure: function(r, o) {
-            Ext.Msg.confirm('Error', 'Unable to get Jira project info. Proceed to Jira and authorize NagUI to make requests?',
-                function(btn) {
-                    if (btn != 'no') {
-                        var currentLocation = window.location;
-                        window.location = currentLocation.origin + NagUI.config.jira.jiraRESTbridge_path + "rest/api/project/" + NagUI.config.jira.project_id + "?callback=" + currentLocation.href;
-                    } else {
-                        NagUI.config.jira.enable = false;
-                    }
-                });
-            return;
-        }
-    });
+    function jiraInit()
+    {
+        Ext.Ajax.request(
+        {
+            url: NagUI.config.jira.jiraRESTbridge_path + 'rest/api/latest/project/' + NagUI.config.jira.project_id,
+            method: 'GET',
+            failure: function(r, o)
+            {
+                Ext.Msg.confirm('Error', 'Unable to get Jira project info. Proceed to Jira and authorize NagUI to make requests?',
+                    function(btn)
+                    {
+                        if (btn != 'no')
+                        {
+                            var currentLocation = window.location;
+                            window.location = currentLocation.origin + NagUI.config.jira.jiraRESTbridge_path + "rest/api/project/" + NagUI.config.jira.project_id + "?callback=" + currentLocation.href;
+                        }
+                        else
+                        {
+                            NagUI.config.jira.enable = false;
+                        }
+                    });
+                return;
+            }
+        });
 
-    function displayDate(value) {
-        if (typeof value == 'number') {
+    }
+
+    function displayDate(value)
+    {
+        if (typeof value == 'number')
+        {
             value = value * 1000;
-            if (value < 5000000) {
+            if (value < 5000000)
+            {
                 return '';
             }
             var dt = new Date(value);
-        } else {
+        }
+        else
+        {
             var dt = value;
         }
         return dt.format('Y-m-d H:i:s (T)');
     }
 
-    function displayState(value) {
-        if (value == 0) {
+    function displayState(value)
+    {
+        if (value == 0)
+        {
             return 'OK';
         }
-        if (value == 1) {
+        if (value == 1)
+        {
             return 'Warning';
         }
-        if (value == 2) {
+        if (value == 2)
+        {
             return 'Critical';
         }
-        if (value == 3) {
+        if (value == 3)
+        {
             return 'Unknown';
         }
     }
@@ -51,7 +72,8 @@ if (NagUI.config.jira.enable) {
             'Output: {plugin_output} \n',
             'Date/Time: {[this.asDate(values.last_hard_state_change)]} \n',
             'Host Notes: {host_notes} \n',
-            'Notes : {notes_url} \n', {
+            'Notes : {notes_url} \n',
+            {
                 asDate: displayDate,
                 stateText: displayState
             }),
@@ -64,7 +86,8 @@ if (NagUI.config.jira.enable) {
             // 'Hostgroups: {groups}<br />',
             'Date/Time: {[this.asDate(values.last_hard_state_change)]} \n',
             'Notes: {notes}  {notes_url} /n',
-            'Comments: ', {
+            'Comments: ',
+            {
                 asDate: displayDate,
                 stateText: displayState,
                 log: NagUI.log
@@ -72,33 +95,41 @@ if (NagUI.config.jira.enable) {
     };
 
 
-    function ackTicket(n, data, callback) {
+    function ackTicket(n, data, callback)
+    {
         var nodeList = new Array();
         var newTicket = {
             description: "",
             summary: "",
-            issuetype: {
+            issuetype:
+            {
                 id: NagUI.config.jira.issuetype
             },
             // status: "1",
-            project: {
+            project:
+            {
                 id: NagUI.config.jira.project_id
             },
-            reporter: {
+            reporter:
+            {
                 name: NagUI.username
             }
         };
 
-        if (n.length > 0) {
+        if (n.length > 0)
+        {
             nodeList = n;
             newTicket.summary = 'Batch Ack: ' + nodeList.length + ' issues: ' + data.ack_text;
             var dt = new Date();
             // newTicket.custom_fields.customfield_10042 = dt.format('d/M/y g:i A');
-        } else {
+        }
+        else
+        {
             nodeList.push(n);
             var dt = n.data.last_hard_state_change;
             // newTicket.custom_fields.customfield_10042 = dt.format('d/M/y g:i A');
-            switch (n.data.state) {
+            switch (n.data.state)
+            {
                 case 3:
                     newTicket.summary = 'Unknown: ';
                     break;
@@ -112,67 +143,86 @@ if (NagUI.config.jira.enable) {
             newTicket.summary += n.data.nagios_type == 'host' ? n.data.name : n.data.host_name + ' : ' + n.data.description;
         }
 
-        Ext.each(nodeList, function(i) {
-            if (i.state == 0) {
+        Ext.each(nodeList, function(i)
+        {
+            if (i.state == 0)
+            {
                 return;
             }
-            if (newTicket.description.length > 12) {
+            if (newTicket.description.length > 12)
+            {
                 newTicket.description += "\n\n";
             }
             newTicket.description += ticketTemplates[i.data.nagios_type].apply(i.data);;
         });
 
-        var diag = Ext.MessageBox.show({
+        var diag = Ext.MessageBox.show(
+        {
             msg: data.ticket_num.match(/(XOPS|PROD|OPS|OA|ENPE)/) != null ? 'updating ticket...' : 'creating ticket...',
             width: 250,
             wait: true,
-            waitConfig: {
+            waitConfig:
+            {
                 interval: 150
             }
         });
-        if (data.ticket_num.match(/(ENPE|TADNU|OPS|OA)/) != null) {
+        if (data.ticket_num.match(/(ENPE|TADNU|OPS|OA)/) != null)
+        {
             var comment = {
                 body: data.ack_text + "\n\n" + newTicket.description,
                 // author: NagUI.username
             };
-            Ext.Ajax.request({
+            Ext.Ajax.request(
+            {
                 url: NagUI.config.jira.jiraRESTbridge_path + 'rest/api/latest/issue/' + data.ticket_num + '/comment',
                 method: 'POST',
                 // timeout: 50000,
                 jsonData: comment,
-                failure: function(r, o) {
+                failure: function(r, o)
+                {
                     Ext.MessageBox.hide();
                     Ext.Msg.alert('Error', 'Unable to update ticket');
                     return;
                 },
-                success: function(r, o) {
-                    if (typeof ackTicket == 'function') {
+                success: function(r, o)
+                {
+                    if (typeof ackTicket == 'function')
+                    {
                         var link = 'https://evernote.jira.com/browse/' + data.ticket_num;
-                        callback(data.ticket_num, diag, {
+                        callback(data.ticket_num, diag,
+                        {
                             title: 'Ticket Updated',
                             msg: data.ticket_num + '<br /><a target="_blank" href="' + link + '">' + link + '</a>'
                         });
                     }
                 }
             });
-        } else {
-            Ext.Ajax.request({
+        }
+        else
+        {
+            Ext.Ajax.request(
+            {
                 url: NagUI.config.jira.jiraRESTbridge_path + 'rest/api/latest/issue',
                 method: 'POST',
                 // timeout: 5000,
-                jsonData: {
+                jsonData:
+                {
                     fields: newTicket
                 },
-                failure: function(r, o) {
+                failure: function(r, o)
+                {
                     Ext.MessageBox.hide();
                     Ext.Msg.alert('Error', 'Unable to create ticket');
                     return;
                 },
-                success: function(r, o) {
+                success: function(r, o)
+                {
                     var lkup = Ext.decode(r.responseText);
-                    if (typeof callback == 'function' && typeof lkup.key != 'undefined') {
+                    if (typeof callback == 'function' && typeof lkup.key != 'undefined')
+                    {
                         var link = 'https://evernote.jira.com/browse/' + lkup.key;
-                        callback(lkup.key, diag, {
+                        callback(lkup.key, diag,
+                        {
                             title: 'Ticket Created',
                             msg: lkup.key + '<br /><a target="_blank" href="' + link + '">' + link + '</a>'
                         });
